2b6fbce42488e9b441182af3ced0d4ff27cbb660
rashik <rkoirala43@gmail.com>
2024-11-13 09:56:04 +0545

    adding app

---
+++ sheets.config.tsx
@@ -0,0 +1,13 @@
+// First, create a config file: src/config/sheets.config.js
+export const SPREADSHEET_ID = "1hjU3-K8vr9akxxcREg4eskPp2BczM6DcrQYB-PIUa18";
+export const CREDENTIALS = {
+  // Copy the contents of your downloaded service account JSON file here
+  type: "service_account",
+  project_id: "sheetapitest-441506",
+  private_key:
+    "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQDAcNj7OaSaUS+P\nyCuWp8rzGlNcToPQH4XJomCEoFup+PouF1KzmH5nN/8VG82oQq6+cR40Xkt2sewO\nIkB7fFVv1S10mWTksUOpGLSKCLdXb13fp7YH2oizUGsgcXw9nVbh7YL1MmfQhdz1\nMEGj9GVEpiWTToKfr25Mwr9tui4zkFSbie8bmQb2aE43BtoNrbOifmiJ6cdO3yrz\nF9qqDtg9dWTXdgCPhZu/VuXN3zMA5ITl2xpjdu2H+/SLxom8CgpGOr1obg1fCl/7\nu3dm2fT9Qr97Nc3npGbWcTdb5Vim0CeGgWfEhsUNcWuFlb59Rl+vj57a0I1fYZPF\n1SKaTUgTAgMBAAECggEAEY7aRUWznn7Uy5YUYAkMK9UVkQD65tTRdJ2zl7Y31tDI\nJsdq/MbnlYxW4jqor3byEZ8phdaqpPhD1OBMKNglxW/aV16dhX4k8bs20bdM3xXi\np0LaiWnnehDWzV19W/PMlw2AGtYQAccM63QNMSPdz38nQ4LwyRgNBMTqCV1o1Lxo\nXH+H8JhaJZ2tXAtYtE9nqJrBKm4PpCSj1IdhkTnvFlpdVqr1q5LSU/g/7mcIii25\nI1w7Zq2v5gkF3t6wWHUXdzoY0mhoORATkc4hBRB7EsRChJZSK4iEUZSeNU8UNk6F\nYQrYlVJIlyj26ovl5uL1FnoxRdak+dCxnMvh8QM6aQKBgQDtHQITRVdQHIQulCTf\nftFj5ddfnCa+t5vHDvq7T609IgAxPQEhu2ywKvMJ9nmZYL7s4jjRvQhnvHWAhfZ/\neUKAFM9sCvJKwTL6H2GQsy2oQh6k6dV2D4817bYit8sZCKHjHNSPFvx9t6ROHlRK\nZebVg6VDEN2BR6yCEuoJx0E+eQKBgQDPxOt3OJtgKALHWLNSyfASK+fXlDV4MgqZ\nl7n2rRiIPGIOVVJ/oevBvSqD2rHqqVGn+tdnSV+/th9Bs1+wj70IU+cAmS0g7002\npKSeBMK/zTcZvVSqZMeOXZdns4Y4y0ur+X7hnl5gtEl7sskhQen0gBLey471Yc8q\nrkB/pOin6wKBgB2bLKIpW1CWbu698qixVgXKMg0vGytfE6VUOnxe43upniqHundy\nxoxsoHITA3XiPZSoXn3Zw43oWyeUEoYyJtqFPqz35O7ulqXiezUtGjtTHi5rQAsA\nwFn0d3R4THSwvj30YJoJPbeY0Xwrj0zG0n7AdEceePnmcCTBpdVYus3JAoGATuxv\nwa8IHqO0QoNoQxegkVaKUDLliM4BhszDAHhKbF37pZQ1rXQVuVehUcr+cLR250d7\nEaomrA2DnBKGU+yWExbEFk8ZfHxKqWorejc2v2M1/hoUklGMGIFm5Sal4ucezKoO\nz6e4FCYID+wfSPnf5FfOU0GiAYZxCrZ8m8BJ/JcCgYA6EqPI/e636tY7s+rGNHAi\nqV86ziUJYoMjTJqAx8o++2bSx0R0McFRkOn2QuLYKz2G21gXTO6Qfz65j0Uv3nut\ns6B4JkUCphnCBdfImvwh2lgYxDKMESTBIYE2k+1Yi7stM+u97sAOCjQ508MVf0br\n0smrrkErsI9eeUPAqt0zFQ==\n-----END PRIVATE KEY-----\n",
+  private_key_id: "1cd9882f16ecb253e228c4dcad7de18d664256ea",
+  client_email: "sheet-integration@sheetapitest-441506.iam.gserviceaccount.com",
+  client_id: "115530878089625793210",
+  // ... rest of your credentials
+};
---
+++ src/GoogleSheetsComponent.tsx
@@ -0,0 +1,74 @@
+import axios from "axios";
+
+const API_URL = import.meta.env.VITE_API_URL;
+
+// Fetch data from Google Sheets
+export const readData = async () => {
+  try {
+    const response = await axios.get(API_URL, { params: { action: "read" } });
+    const fetchedData = response.data.map((row: string[]) => ({
+      projectName: row[0],
+      projectLeader: row[1],
+      monday: row[2],
+      tuesday: row[3],
+      wednesday: row[4],
+      thursday: row[5],
+      friday: row[6],
+    }));
+    return fetchedData;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+};
+
+// Function to add data to Google Sheets
+export const addData = async (name: string, email: string) => {
+  try {
+    await axios.post(API_URL, null, {
+      params: {
+        action: "create",
+        projectName: name,
+        projectLeader: email,
+      },
+    });
+    console.log("Data added successfully");
+  } catch (error) {
+    console.error("Error adding data:", error);
+  }
+};
+
+// Function to update a specific cell in Google Sheets
+export const updateData = async (
+  row: number,
+  column: number,
+  value: string
+) => {
+  try {
+    await axios.post(API_URL, null, {
+      params: {
+        action: "update",
+        row: row,
+        column: column,
+        value: value,
+      },
+    });
+    console.log("Cell updated successfully");
+  } catch (error) {
+    console.error("Error updating cell:", error);
+  }
+};
+
+// Function to delete data from Google Sheets
+export const deleteData = async (id: number) => {
+  try {
+    await axios.post(API_URL, null, {
+      params: {
+        action: "delete",
+        id: id,
+      },
+    });
+    console.log("Data deleted successfully");
+  } catch (error) {
+    console.error("Error deleting data:", error);
+  }
+};
