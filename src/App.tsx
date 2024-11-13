a07e34bc0ee4d29a1dc80fff33d584b07eed2af3
adwaitupadhyaya <adwait627@gmail.com>
2024-11-13 16:08:19 +0545

    feat:slider touch up

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,11 @@ import {
   SheetTrigger,
 } from "@/components/ui/sheet";
 import { Button } from "./components/ui/button";
+import Shift from "./components/Shift";
 
 const App = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isSheetOpen, setIsSheetOpen] = useState(false);
 
   return (
     <>
@@ -21,7 +23,7 @@ const App = () => {
           <SheetTrigger>
             <Button
               variant="outline"
-              className={`bg-black rounded-none hover:bg-gray-500 hover:text-white text-white absolute -right-5 top-10 transform -translate-y-1/2 rotate-90 transition-transform duration-300 ${
+              className={`bg-black rounded-none hover:bg-gray-500 hover:text-white text-white absolute -right-7 top-[3rem] transform -translate-y-1/2 rotate-90 transition-transform duration-300 h-10 w-24 ${
                 isOpen ? "translate-x-[-60vw]" : ""
               }`}
             >
@@ -32,6 +34,27 @@ const App = () => {
           {/* Slide-in Sheet Content */}
           <SheetContent className="w-[60%] h-full">
             <Roster />
+          </SheetContent>
+        </div>
+      </Sheet>
+
+      <Sheet open={isSheetOpen} onOpenChange={setIsSheetOpen}>
+        <div className="relative">
+          {/* Bookmark Button */}
+          <SheetTrigger className="w-full flex justify-end">
+            <Button
+              variant="outline"
+              className={`bg-black rounded-none hover:bg-gray-500 hover:text-white text-white absolute -right-7 top-[8rem] transform -translate-y-1/2 rotate-90 transition-transform duration-300 h-10 w-24 ${
+                isSheetOpen ? "translate-x-[-60vw]" : ""
+              }`}
+            >
+              Shift
+            </Button>
+          </SheetTrigger>
+
+          {/* Slide-in Sheet Content */}
+          <SheetContent className="w-[60%] h-full">
+            <Shift />
           </SheetContent>
         </div>
       </Sheet>
